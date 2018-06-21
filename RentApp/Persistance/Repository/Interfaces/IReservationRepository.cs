770be8477a7ca2ec8f0729b497af72934ecad6dc
Pedja <sadnop95@gmail.com>
2018-06-21 19:35:21 +0200

    rezervacije provera

--- RentApp/Persistance/Repository/Interfaces/IReservationRepository.cs
+++ RentApp/Persistance/Repository/Interfaces/IReservationRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using RentApp.Models.Entities;
 
 namespace RentApp.Persistance.Repository.Interfaces
@@ -6,5 +7,6 @@ namespace RentApp.Persistance.Repository.Interfaces
     public interface IReservationRepository : IRepository<Reservation, int>
     {
         bool IsReserved(Vehicle vehicle, DateTime fromThisTime);
+        List<Reservation> GetAllWithSecificVehicle(int vehicle);
     }
 }
